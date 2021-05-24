2d9a4ab2fb79c903ce7de5ef4d837dcd8b1d022e
Anne Lepetit <lepetit.anne@gmail.com>
2021-05-24 17:45:42 +0200

    models

---
+++ db/migrate/20210524153702_create_authors.rb
@@ -0,0 +1,11 @@
+class CreateAuthors < ActiveRecord::Migration[6.0]
+  def change
+    create_table :authors do |t|
+      t.string :first_name
+      t.string :last_name
+      t.text :bio
+
+      t.timestamps
+    end
+  end
+end
---
+++ test/models/recommended_book_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RecommendedBookTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
