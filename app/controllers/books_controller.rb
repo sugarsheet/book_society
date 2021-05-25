61b5473347ad2376912d4ffb08cdeca186edc88d
Mathieu Chaplain <mathieuchaplain@gmail.com>
2021-05-25 16:03:36 +0200

    pgsearch2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,17 @@
 class BooksController < ActionController::Base
 
   def index
-  Book.global_search(params[:query])
-  #  call book.global_search in rails c
+    @books = Book.order(title: :desc)
+
+    if params[:query].present?
+      # @books = @books.where('title ILIKE ?', "%#{params[:query]}%")
+      @books = Book.global_search(params[:query])
+    end
+
+    respond_to do |format|
+      format.html # Follow regular flow of Rails
+      format.text { render partial: 'list.html', locals: { books: @books } }
+    end
   end
 
 end
