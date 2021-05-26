1edd13bfd9f0ffb194bae73bfefafd4f7720b3a6
Audrey Le Moine <audreylm33@gmail.com>
2021-05-26 18:07:08 +0200

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get '/dashboard', to: 'pages#dashboard'
+  get '/signup', to: 'pages#signup'
   resources :books, only: [:index, :show] do
     collection do
       get :recommended
