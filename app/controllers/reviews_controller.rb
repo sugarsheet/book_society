5b59c0fd20a0e448bd27ee683efb98290b73e3ab
Jiwon EUN <jiwon.eun@gmail.com>
2021-05-26 17:30:07 +0200

    review add and css - missing create

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,27 +1,26 @@
 class ReviewsController < ApplicationController
 
   def new
-   @review = Review.new
+    @book = Book.find(params[:book_id])
+    @review = Review.new
   end
 
-
   def create
-    @book = Book.find(params[:books_id])
+    @book = Book.find(params[:book_id])
     @review = Review.new(review_params)
     @review.book = @book
     @review.user = current_user
     if @review.save
-    redirect_to book_path(@book), notice: "Your review is added!"
-
+      redirect_to book_path(@book), notice: "Thanks for your contribution"
     else
-    redirect_to dashboard_path(@book)
+      redirect_to dashboard_path(@book)
     end
   end
 
   private
 
   def review_params
-    params.require(:books).permit(:title, :description, :isbn)
+    params.require(:review).permit(:title, :description, :isbn)
   end
 
 end
