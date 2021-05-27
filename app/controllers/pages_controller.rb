4fdd3bae89cda82d7f313a6e7f5c4a1113889782
Jiwon EUN <jiwon.eun@gmail.com>
2021-05-27 15:25:01 +0200

    jiji

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,20 +13,17 @@ class PagesController < ApplicationController
       @books = Book.global_search(params[:query])
     end
 
-    respond_to do |format|
-      format.html # Follow regular flow of Rails
-      format.text { render partial: 'list.html', locals: { books: @books } }
-    end
+    # respond_to do |format|
+    #   format.html # Follow regular flow of Rails
+    #   format.text { render partial: 'list.html', locals: { books: @books } }
+    # end
   end
 
   def signup
     if user_signed_in?
       redirect_to dashboard_path(@book)
-
     else
-
      redirect_to signup_path
-
     end
   end
 end
