cf00b56436db994b71ce3b7cdffa40dad9546ed7
Jiwon EUN <jiwon.eun@gmail.com>
2021-05-30 22:10:25 +0200

    404 and 500 error pages setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
+  
   root to: 'pages#home'
   get '/dashboard', to: 'pages#dashboard'
   get '/signup', to: 'pages#signup'
